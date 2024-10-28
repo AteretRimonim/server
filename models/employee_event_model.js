97bcc5cd08c06c2d50d2bcb88b57adbd37d4bc5f
hadassa <h0556737655@gmail.com>
2024-10-28 11:29:54 +0200

    Corrections after the code review.

--- models/employee_event_model.js
+++ models/employee_event_model.js
@@ -10,22 +10,24 @@ employee_id:{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Employee', 
     required: true,
-   },
+},
 event_id:{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Event', 
     required: true,
-   },
-bonus_given:{
+},
+   bonus_given: {
+    type: Number,
+    required: true,
+},
+
+   remarks: {
     type: String,
     required: true,
     trim: true,
-   },
-remarks:{
-    type: String,
-    required: true,
-    trim: true,
-   }
+    maxlength: 300, 
+}
+
 });
 
 module.exports = mongoose.model('EmployeeEvent', employeeEventSchemaSchema);;
